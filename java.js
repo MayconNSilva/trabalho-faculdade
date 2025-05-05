f9c1cc11e777a3ac5a6677faa29f828ab4eca050
Maycon Silva <wbxwf4@gmail.com>
2025-05-05 17:28:15 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,6 @@
+const botao = document.getElementById('botao');
+const mensagem = document.getElementById('mensagem')
+
+botao.addEventListener('click', function() {
+    mensagem.textContent = 'Persista. Nunca desista dos seus sonhos 😁';
+});
